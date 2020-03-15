fae179e547f4023cfa826e48c64b685d5ddac326
Justyna <justyna.strawiak@gmail.com>
2020-03-15 01:39:45 +0100

    trees destroyed on trigger enter

---
+++ Disaster/Assets/Scripts/TreeObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeObject : MonoBehaviour
+{
+    public GameObject tree;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("hiiiiit");
+            Destroy(tree);
+            this.tag = "Untagged";
+        }
+    }
+}
