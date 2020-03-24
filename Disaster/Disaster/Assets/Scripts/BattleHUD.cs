bf51b0531616c4415482a81269a344be2589b5f2
Justyna <justyna.strawiak@gmail.com>
2020-03-24 15:15:11 +0100

    battle system

---
+++ Disaster/Disaster/Assets/Scripts/BattleHUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHUD : MonoBehaviour
+{
+    public Text nameText;
+    public Text levelText;
+    public Slider hpSlider;
+
+    public void SetupHud(Unit unit)
+    {
+        nameText.text ="Name: " + unit.unitName;
+        levelText.text = "LvL: " + unit.unitLvl;
+        hpSlider.maxValue = unit.maxHp;
+        hpSlider.value = unit.currentHp;
+    }
+
+    public void SetHP(int hp)
+    {
+        hpSlider.value = hp;
+    }
+
+    public void Heal(int hp)
+    {
+        hpSlider.value += hp;
+    }
+}
---
+++ Disaster/Disaster/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState state;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    public Text dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+        GameObject playerGo = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGo.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "Watch out! " + enemyUnit.unitName + " approaches...";
+
+        playerHUD.SetupHud(playerUnit);
+        enemyHUD.SetupHud(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHp);
+        dialogueText.text = "The attack is succesful!";
+
+        yield return new WaitForSeconds(2f);
+
+        if(isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action:";
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+            dialogueText.text = "You won the battle!";
+        else if (state == BattleState.LOST)
+            dialogueText.text = "You lost the battle!";
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHp);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+
+    }
+
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerHeal());
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.currentHp += 15;
+        playerHUD.Heal(15);
+        dialogueText.text = "The heal is succesful!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+}
