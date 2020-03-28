7f84598f7bcdbcb84e6905d53e1b848d881884fa
Oliwia <ofrontczak2547@gmail.com>
2020-03-28 19:20:01 +0100

    Camera movement

---
+++ Disaster/Disaster/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed;
+    //how close we need to get to the endge of the screen to move
+    public float cameraMovementBorder;
+    public Vector2 cameraLimit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // current position of our camera
+        Vector3 pos = transform.position;
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - cameraMovementBorder)
+        {
+            pos.z += cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= cameraMovementBorder)
+        {
+            pos.z -= cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - cameraMovementBorder)
+        {
+            pos.x += cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= cameraMovementBorder)
+        {
+            pos.x -= cameraSpeed * Time.deltaTime;
+        }
+
+        //scrolling?
+
+        pos.x = Mathf.Clamp(pos.x, -cameraLimit.x, cameraLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -cameraLimit.y, cameraLimit.y);
+
+        transform.position = pos;
+    }
+}
