cda36e2e167808eb6b100845389e6d62e15d59d3
Oliwia <ofrontczak2547@gmail.com>
2020-04-02 13:07:38 +0200

    Camera

--- Disaster/Disaster/Assets/Scripts/CameraController.cs
+++ Disaster/Disaster/Assets/Scripts/CameraController.cs
@@ -3,31 +3,38 @@
 public class CameraController : MonoBehaviour
 {
     public float cameraSpeed;
+    public float smoothSpeed = 0.125f;
+    public Transform playerPrefab;
+    public Vector3 offset;
+    Player target;
     //how close we need to get to the endge of the screen to move
-    public float cameraMovementBorder;
     public Vector2 cameraLimit;
 
+    private void Start()
+    {
+        target = GameObject.FindObjectOfType<Player>();
+    }
     // Update is called once per frame
     void Update()
     {
         // current position of our camera
         Vector3 pos = transform.position;
-        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - cameraMovementBorder)
+        if(Input.GetKey("w"))
         {
             pos.z += cameraSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("s") || Input.mousePosition.y <= cameraMovementBorder)
+        if (Input.GetKey("s"))
         {
             pos.z -= cameraSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - cameraMovementBorder)
+        if (Input.GetKey("d"))
         {
             pos.x += cameraSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("a") || Input.mousePosition.x <= cameraMovementBorder)
+        if (Input.GetKey("a"))
         {
             pos.x -= cameraSpeed * Time.deltaTime;
         }
@@ -38,5 +45,14 @@ public class CameraController : MonoBehaviour
         pos.z = Mathf.Clamp(pos.z, -cameraLimit.y, cameraLimit.y);
 
         transform.position = pos;
+    }
+
+    public void FindPlayer()
+    {
+        Vector3  desiredPosition = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z) + offset;
+        //Vector3 smoothPosition = Vector3.Lerp(transform.position,desiredPosition,smoothSpeed*Time.deltaTime);
+
+        transform.position = desiredPosition;
+
     }
 }
